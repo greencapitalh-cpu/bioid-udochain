29011fc0540bb3764ecaf483239a89eec183d42c
greencapitalh-cpu <greencapitalh@gmail.com>
2025-11-08 12:29:21 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
+import Enroll from "./pages/Enroll";
+import Verify from "./pages/Verify";
 
 export default function App() {
   return (
     <AuthProvider>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/enroll" element={<Enroll />} />
+          <Route path="/verify" element={<Verify />} />
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
