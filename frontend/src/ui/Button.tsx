aa6f588e68bf2892370d84d4fcd7363a3dd62923
greencapitalh-cpu <greencapitalh@gmail.com>
2025-11-09 01:29:07 -0400

    Update and rename src/ui/Button.tsx to frontend/src/ui/Button.tsx

---
+++ frontend/src/ui/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Button(
+  props: React.ButtonHTMLAttributes<HTMLButtonElement>
+) {
+  const { className = "", ...rest } = props;
+  return (
+    <button
+      {...rest}
+      className={`btn btn-primary ${className}`}
+    />
+  );
+}
